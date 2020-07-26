91ed6ed422cd6306afcd88c932f79bc6391e7e8a
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-07-26 11:31:14 -0300

    Events Stream

--- src/main/java/com/talissonmelo/webflux/controller/PlaylistController.java
+++ src/main/java/com/talissonmelo/webflux/controller/PlaylistController.java
@@ -1,19 +1,25 @@
 package com.talissonmelo.webflux.controller;
 
+import java.time.Duration;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.talissonmelo.webflux.document.Playlist;
 import com.talissonmelo.webflux.service.PlaylistService;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
 
-//@RestController
-//@RequestMapping(value = "/playlist")
+@RestController
+@RequestMapping(value = "/playlist")
 public class PlaylistController {
 
 	@Autowired
@@ -32,5 +38,14 @@ public class PlaylistController {
 	@PostMapping
 	public Mono<Playlist> insert(@RequestBody Playlist playlist){
 		return service.insertPlaylist(playlist);
+	}
+	
+	@GetMapping(value="/playlist/events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	public Flux<Tuple2<Long, Playlist>> getPlaylistByWebflux(){
+
+		Flux<Long> interval = Flux.interval(Duration.ofSeconds(5));
+        Flux<Playlist> playlistFlux = service.findAll();
+        return Flux.zip(interval, playlistFlux);
+        
 	}
 }
--- src/main/java/com/talissonmelo/webflux/reactor/PlaylistRouter.java
+++ src/main/java/com/talissonmelo/webflux/reactor/PlaylistRouter.java
@@ -5,21 +5,20 @@ import static org.springframework.web.reactive.function.server.RequestPredicates
 import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
 import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
-@Configuration
+//@Configuration
 public class PlaylistRouter {
 
-	@Bean
+	//@Bean
 	public RouterFunction<ServerResponse> router(PlaylistHandler playlistHandler){
 		return RouterFunctions
 				.route(GET("/playlist").and(accept(MediaType.APPLICATION_JSON)), playlistHandler::findAll)
 				.andRoute(GET("/playlist/{id}").and(accept(MediaType.APPLICATION_JSON)), playlistHandler::findById)
-				.andRoute(POST("/playlist").and(accept(MediaType.APPLICATION_JSON)), playlistHandler::insert);
+				.andRoute(POST("/playlist").and(accept(MediaType.APPLICATION_JSON)), playlistHandler::insert
+						);
 	}
 }
