c9977b0342268ccc9e1230fd6b963bc417af4eb4
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-07-26 09:01:42 -0300

    Criando a Controller

---
+++ src/main/java/com/talissonmelo/webflux/controller/PlaylistController.java
@@ -0,0 +1,38 @@
+package com.talissonmelo.webflux.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.talissonmelo.webflux.document.Playlist;
+import com.talissonmelo.webflux.service.PlaylistService;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping(value = "/playlist")
+public class PlaylistController {
+
+	@Autowired
+	private PlaylistService service;
+	
+	@GetMapping
+	public Flux<Playlist> findAll(){
+		return service.findAll();
+	}
+	
+	@GetMapping(value = "/{id}")
+	public Mono<Playlist> findById(@PathVariable String id){
+		return service.findById(id);
+	}
+	
+	@PostMapping
+	public Mono<Playlist> insert(@RequestBody Playlist playlist){
+		return service.insertPlaylist(playlist);
+	}
+}
--- src/main/java/com/talissonmelo/webflux/service/impl/PlaylistServiceImpl.java
+++ src/main/java/com/talissonmelo/webflux/service/impl/PlaylistServiceImpl.java
@@ -1,6 +1,7 @@
 package com.talissonmelo.webflux.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.talissonmelo.webflux.document.Playlist;
 import com.talissonmelo.webflux.repository.PlaylistRepository;
@@ -9,6 +10,7 @@ import com.talissonmelo.webflux.service.PlaylistService;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+@Service
 public class PlaylistServiceImpl implements PlaylistService {
 
 	@Autowired
